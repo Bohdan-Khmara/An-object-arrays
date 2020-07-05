cede4ee409b4267eb6a13a2e9e3dfde7db144a94
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:55:08 +0300

    The function of removing an element from an array at the specified index. Task 6

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
- * The function of adding a new element to the array at the specified index. Task 5
+ * The function of removing an element from an array at the specified index. Task 6
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function addElem(i, newElem) {
-    arr.splice(i, 0, newElem);
+function deleteElem(i, deleteElem) {
+    arr.splice(i, deleteElem);
     return arr;
 }
 
-addElem(0, 0);
+deleteElem(0, 1);
