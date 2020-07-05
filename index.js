df2204dc60d67290329adaa726325c934bfc8bde
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:53:19 +0300

    The function takes an array and returns its maximum element. Task 4

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 /**
- * The function takes an array and returns the sum of all elements of the array. Task 3
+ * The function takes an array and returns its maximum element. Task 4
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function sum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum;
+function maxValue(arr) {
+    return Math.max(...arr);
 }
 
-sum(arr);
+maxValue(arr);
