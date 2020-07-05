dd555ef26cf1d61ebca4bd73b10b3b8d61ad3bd8
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:54:07 +0300

    The function of adding a new element to the array at the specified index. Task 5

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
- * The function takes an array and returns its maximum element. Task 4
+ * The function of adding a new element to the array at the specified index. Task 5
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function maxValue(arr) {
-    return Math.max(...arr);
+function addElem(i, newElem) {
+    arr.splice(i, 0, newElem);
+    return arr;
 }
 
-maxValue(arr);
+addElem(0, 0);
