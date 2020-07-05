ec6cdf389934d471f773f4d8b19ccc805ba9b173
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:49:43 +0300

    The function takes an array and displays it on the screen. Task 1

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * The function takes an array and displays it on the screen. Task 1
+ * @type {number[]}
+ */
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function display(arr) {
+    return arr;
+}
+
+display(arr);
