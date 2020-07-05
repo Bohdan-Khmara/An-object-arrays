000a0a9b9e1098b6676159bf73b5bda154b8793f
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:50:53 +0300

    The function takes an array and outputs only even elements. Task 2

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /**
- * The function takes an array and displays it on the screen. Task 1
+ * The function takes an array and outputs only even elements. Task 2
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function display(arr) {
-    return arr;
+function evenNumbers(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+        if (i % 2 === 0) {
+            alert(i);
+        }
+    }
 }
 
-display(arr);
+evenNumbers(arr);
