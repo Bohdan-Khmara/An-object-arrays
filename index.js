47ba7ce43db56e45f843b9777e2f8222fea6ecb4
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-05 21:57:30 +0300

    returns a new array in which all elements from two arrays are collected without repetition. Task 8

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /**
- * The function takes 2 arrays and returns a new array in which all elements from two arrays are collected without repetition. Task 7
+ * returns a new array in which all elements from two arrays are collected without repetition. Task 8
  * @type {(string|number)[]}
  */
 let array1 = ['a', 2, 'b', 4, 5, 6, 'c', 8, 'D', 10];
 let array2 = [10, 2, 5, 1, 7, 9, 3, 4, 6, 8];
-let finalArr = array1.concat(array2);
+let finalArr = [];
 
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
+function comparison(array1, array2) {
+    finalArr = array1.concat(array2);
+    finalArr = finalArr.filter(function (item, index, arr) {
+        return arr.indexOf(item) === index;
+    });
+    return finalArr;
 }
 
-let unique = finalArr.filter(onlyUnique);
-alert(unique);
+comparison(array1, array2);
